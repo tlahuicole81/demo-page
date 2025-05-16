23568becb1c63bd28bf24216abdc6dad7155d212
tlahuicole81 <el_caballero_jaguar@hotmail.com>
2025-05-16 03:24:38 -0600

    secc_even_comp.js

--- secc_even_comp.js
+++ secc_even_comp.js
@@ -2,16 +2,21 @@
 
 let modoEdicionEvento = false;
 let nombreOriginalEvento = "";
+let opcEventos = [];
 
 const beliminarEvento = $("eliminarEvento");
 const bsalvarEvento = $("salvarEvento");
 
 const eventosSelect = $("eventosSelect");
 const detallesEventos = $("eventosDetails");
+
 /* 
 */
 function inicializarEventos() {
   cargaDatosSlctEventos();
+  document.getElementById("fotoEventoInput").value = "";
+  validarArchivoImagen("fotoEventoInput", "previewCompEvento");
+  $('previewCompEvento').innerHTML = "";
   // ComboBox para la selección del evento
   eventosSelect.addEventListener("change", () => {
     const seleccionEvento = eventosSelect.value;
@@ -23,7 +28,8 @@ function inicializarEventos() {
         break;
       case "Nuevo evento":
         console.log("Nuevo evento...");
-        const eventoSinDatos = creaEventoVacIo();
+        //const eventoSinDatos = creaEventoVacIo();
+        const eventoSinDatos = creaObjetoVacio(fieldMappingEventos);
         populateFormEventos(eventoSinDatos);
         detallesEventos.style.display = "block";
         modoEdicionEvento = false;
@@ -45,37 +51,25 @@ function inicializarEventos() {
   });
 
   bsalvarEvento.addEventListener("click", async () => {
+    if (!modoEdicionEvento && !imagenProcesadaOK) {
+      //console.log("Si estamos en modo Nuevo evento y no hay imagen disponible");
+      mostrarToast("Por favor selecciona una imagen");
+      return;
+    }
+
     // Primero revisar si hay algo para enviar    
     if (confirm(`¿Enviar la información?`)) {
       deshabilitaBotonesPArteEvento();
-      const datos = {};
-      // Recorremos el fieldMappingEventos
-      Object.entries(fieldMappingEventos).forEach(([idCampo, nombreHeader]) => {
-        const valor = document.getElementById(idCampo)?.value || "";
-        datos[nombreHeader] = valor;
-      });
-      // Campos extra fuera del mapping
-      datos["Usuario Organizador"] = elCorreo;
-      datos["Asociación Organizador"] = nombreAsociacion;
-      //
-      let destino = "EditarEvento"; // presuponemos
+      const datos = camposHtmlAObjeto(fieldMappingEventos);
+      //                condición ? expresiónSiVerdadero : expresiónSiFalso     
+      datos["Imagen"] = imagenProcesadaOK ? base64Comprobante : "";
+
+      let destino = "NuevoEvento"; // presuponemos
       if (modoEdicionEvento) {
-        // Se editaron los campos para los eventos. La imagen es opcional
-        // condición ? expresiónSiVerdadero : expresiónSiFalso
-        datos["Imagen"] = imagenProcesadaOK ? base64Comprobante : "";
-        // Sin embargo es necesario enviar el nombre original del evento
+        // Estamos en modo edición, en este modo se puede enviar o no una nueva imagen
+        // Es necesario enviar el nombre original del evento
+        destino = "EditarEvento";
         datos["nombreOriginal"] = nombreOriginalEvento;
-      } else {
-        destino = "NuevoEvento";
-        if (imagenProcesadaOK) {
-          // La imagen base64 
-          datos["Imagen"] = base64Comprobante;
-        } else {
-          // Acá la imagen es obligatoria
-          habilitaBotonesPArteEvento();
-          mostrarToast("Por favor selecciona una imagen");
-          return;
-        }
       }
       const val = JSON.stringify({ destino, datos });
       console.log(val);
@@ -92,11 +86,15 @@ function inicializarEventos() {
       deshabilitaBotonesPArteEvento();
       console.log("Sí, se quiere eliminar");
       const destino = "EliminarEvento";
-      const objetivo = nombreOriginalEvento
+      const objetivo = nombreOriginalEvento;
       const val = JSON.stringify({ destino, objetivo });
       console.log(val);
       const resp = await enviarPOST(val);
       alert(resp.message);
+      if(resp.success){
+        // Se eliminará la información del evento del array principal
+        eliminaEvento();
+      }
       habilitaBotonesPArteEvento();
     }
   });
@@ -104,14 +102,7 @@ function inicializarEventos() {
 
   document.querySelectorAll('.edit-field-evento').forEach(field => {
     field.addEventListener('input', () => {
-      // Obtenemos la ID del control para poder configurar el nombre del valor en el JSON
-      //let nmb = field.id.toString().replace("Input", "");
-      //nmb = nmb[0].toUpperCase() + nmb.slice(1);
-      //console.log("El control del cambio fue: ", field.name);
-      // Se comparan valores, 
-      // El valor constante para identificar dónde está ocurriendo la modificación es por
-      // medio del comboBox de selección de eventos, ya que tiene la opción de "Nuevo evento"
-      // y los eventos descargados.
+
       console.log("Selección hecha por el usuario: ", eventosSelect.value);
       if (eventosSelect.value === "Nuevo evento") {
         if (field.value === "") {
@@ -124,19 +115,7 @@ function inicializarEventos() {
       } else {
         // Comparamos los valores ingresados        
       }
-      /*if (initialData[nmb] !== field.value) {
-        // El usuario modificó un campo, se resalta cambiando el color del borde.
-        field.style.borderColor = "#ff9800";
-        contCambios++;
-        if (contCambios === 1) {
-          // Ocurrió la primera modificación, cuando esto sucede se deshabilitan
-          // los botones...
-        }
-      } else {
-        // El usuario regresó al valor previo del cambio en el campo
-        field.style.borderColor = "#ccc";
-        contCambios--;
-      }*/
+
     });
   });
 }
@@ -144,12 +123,10 @@ function inicializarEventos() {
 // Cuando el usuario modificó la información del evento se actualizará
 // el objeto "eventos" que es donde está almacenada a información.
 function modificarArr(nuevaImg) {
-  console.log("La nueva imagen: ", nuevaImg);
-  // nombreOriginalEvento
   // Buscar el objeto evento en el array
   const evento = eventos.find(e => e["Nombre del evento"] === nombreOriginalEvento);
   if (evento) {
-    // evento[campo] = nuevoValor;
+    // Como sí se encontró el objeto entonces vamos a modificar los valores.
     Object.entries(fieldMappingEventos).forEach(([fieldId, config]) => {
       if (fieldId === "eventPoster") {
         if (nuevaImg !== "")
@@ -161,9 +138,23 @@ function modificarArr(nuevaImg) {
         evento[config] = viewField.value;
       }
     });
-    // 
-    cargaDatosSlctEventos();
+    //     
+  } else {
+    // Se agregará un nuevo evento
+    const datos = camposHtmlAObjeto(fieldMappingEventos);
+    datos["Imagen"] = (nuevaImg !== "") ? nuevaImg : "";
+    eventos.push(datos);
   }
+  cargaDatosSlctEventos();
+}
+
+function eliminaEvento(){
+// Con .filter() se recorre todo el array
+  const eventosActualizados = eventos.filter(evento => evento["Nombre del evento"] !== nombreOriginalEvento);
+  //console.log("La actualización de los eventos: ", eventosActualizados);
+  eventos.length = 0; // Se vacía el array original
+  eventos.push(...eventosActualizados); // Rellenas con el nuevo contenido
+  cargaDatosSlctEventos();
 }
 
 function cargaDatosSlctEventos() {
@@ -206,14 +197,6 @@ function habilitaBotonesPArteEvento() {
   eventosSelect.dispatchEvent(new Event('change'));
 }
 
-function creaEventoVacIo() {
-  const eventoVac1o = {};
-  Object.entries(fieldMappingEventos).forEach(([fieldId, config]) => {
-    eventoVac1o[config] = "";
-  });
-  return eventoVac1o;
-}
-
 function populateFormEventos(selectedEvento) {
   //console.log("Examinando estructura evento: ", selectedEvento);  
   Object.entries(fieldMappingEventos).forEach(([fieldId, config]) => {
@@ -222,37 +205,71 @@ function populateFormEventos(selectedEvento) {
     switch (fieldId) {
       case "eventPoster":
         //console.log("Sí, acá va el poster: ", selectedEvento[config]);
-        if (selectedEvento[config] !== "") {
-          const match = selectedEvento[config].match(/\/d\/([a-zA-Z0-9_-]+)\//);
-          if (match && match[1]) {
-            const fileId = match[1];
-            const iframe = document.createElement('iframe');
-            iframe.src = `https://drive.google.com/file/d/${fileId}/preview`;
-            iframe.style.border = "none";
-
-            const contenedor = $('previewCompEvento');
-            // Eliminar iframe anterior si ya existe
-            const anterior = contenedor.querySelector("iframe");
-            if (anterior) contenedor.removeChild(anterior);
-            // Eliminar img anterior si ya existe
-            const otraAnterior = contenedor.querySelector("img");
-            if (otraAnterior) contenedor.removeChild(otraAnterior);
-            contenedor.appendChild(iframe);
-          }
-        } else {
-          // Se tiene que limpiar la posible imagen previa
-          $("previewCompEvento").innerHTML = "";
-          document.getElementById("fotoEventoInput").value = "";
-        }
+        imagenContenedor('previewCompEvento', "fotoEventoInput", selectedEvento[config]);
         break;
       case "eventDuration":
         viewField.value = selectedEvento[config];
         break;
       default:
         viewField.value = selectedEvento[config] || '';
-    }
-    base64Comprobante = null;
-    imagenProcesadaOK = false;
+    }    
     nombreOriginalEvento = "";
   });
+}
+
+/* Esta función compartida permite visualizar una imagen con iframe tomando la url proporcionada por GAS  
+*/
+function imagenContenedor(elContenedor, elInput, urlImg) {
+  const contenedor = $(elContenedor);
+  const laInput = $(elInput);
+  if (urlImg !== "") {
+    const match = urlImg.match(/\/d\/([a-zA-Z0-9_-]+)\//);
+    if (match && match[1]) {
+      const fileId = match[1];
+      const iframe = document.createElement('iframe');
+      iframe.src = `https://drive.google.com/file/d/${fileId}/preview`;
+      iframe.style.border = "none";
+
+      // Eliminar iframe anterior si ya existe
+      const anterior = contenedor.querySelector("iframe");
+      if (anterior) contenedor.removeChild(anterior);
+      // Eliminar img anterior si ya existe
+      const otraAnterior = contenedor.querySelector("img");
+      if (otraAnterior) contenedor.removeChild(otraAnterior);
+      contenedor.appendChild(iframe);
+    }
+  }
+  else {
+    // Se tiene que limpiar la posible imagen previa
+    contenedor.innerHTML = "";
+    laInput.value = "";
+  }
+  base64Comprobante = null;
+  imagenProcesadaOK = false;
+}
+
+/* Función compartida
+*/
+function creaObjetoVacio(fieldMapping) {
+  let objVacio = {};
+  Object.entries(fieldMapping).forEach(([fieldId, config]) => {
+    objVacio[config] = "";
+  });
+  return objVacio;
+}
+
+function camposHtmlAObjeto(fieldMapping) {
+  const datos = {};
+  // Recorremos el fieldMappingEventos.
+  // idCampo es el nombre del control html  ||| nombreHeader es el valor del campo recibido en el JSON
+  Object.entries(fieldMapping).forEach(([idCampo, nombreHeader]) => {
+    //            condición ? expresiónSiVerdadero : expresiónSiFalso
+    const valor = document.getElementById(idCampo)?.value || "";
+    datos[nombreHeader] = valor;
+  });
+  // Campos extra fuera del mapping
+  datos["Usuario Organizador"] = elCorreo;
+  datos["Asociación Organizador"] = nombreAsociacion;
+
+  return datos;
 }
